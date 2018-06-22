c45a10e49827eabceb2a79d8bebdc85f83173a1a
mamori017 <mamori017@outlook.jp>
2018-06-22 16:34:49 +0900

    First commit

---
+++ UnitTest/LogTest.cs
@@ -0,0 +1,52 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Common;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UnitTest
+{
+    [TestClass]
+    public class LogTest
+    {
+
+        [TestMethod]
+        public void TestMethod1()
+        {
+            string strFile = @".\errorLog.txt";
+
+            Exception objEx = new Exception("Test");
+
+            if (File.Exists(strFile))
+            {
+                File.Delete(strFile);
+            }
+
+            Common.Log.ExceptionOutput(objEx, strFile);
+
+            Assert.AreEqual(true, File.Exists(strFile));
+
+            StreamReader objReader = null;
+
+            if (File.Exists(strFile))
+            {
+                objReader = new StreamReader(strFile);
+                Assert.AreEqual("System.Exception: Test", objReader.ReadLine());
+
+            }
+
+            if (objReader != null)
+            {
+                objReader.Close();
+            }
+
+            if (File.Exists(strFile))
+            {
+                File.Delete(strFile);
+            }
+        }
+    }
+}
