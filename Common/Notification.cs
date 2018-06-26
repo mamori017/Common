8b66ad95149de9471eaa9b002b4ed0adf6d36c59
mamori017 <mamori017@outlook.jp>
2018-06-26 14:06:36 +0900

    Add Notification, Process class

---
+++ Common/Notification.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.IO;
+using Windows.UI.Notifications;
+
+namespace Common
+{
+    public static class Notification
+    {
+        /// <summary>
+        /// ShowNotify
+        /// </summary>
+        public static void ShowNotify(String strLine_1, string strLine_2, Boolean blnExeFlg, string strProductName)
+        {
+            try
+            {
+                var tmpl = ToastTemplateType.ToastImageAndText02;
+                var xml = ToastNotificationManager.GetTemplateContent(tmpl);
+                var images = xml.GetElementsByTagName("image");
+                var src = images[0].Attributes.GetNamedItem("src");
+                var texts = xml.GetElementsByTagName("text");
+                var toast = new ToastNotification(xml);
+                var notify = ToastNotificationManager.CreateToastNotifier(strProductName);
+                string filePath = "";
+
+                if(blnExeFlg == true)
+                {
+                    filePath = Define.NotifyIconPath;
+                }
+                else
+                {
+                    filePath = Define.NotifyStopIconPath;
+                }
+
+                src.InnerText = "file:///" + Path.GetFullPath(filePath);
+
+                texts[0].AppendChild(xml.CreateTextNode(strLine_1));
+                texts[1].AppendChild(xml.CreateTextNode(strLine_2));
+
+                notify.Show(new ToastNotification(xml));
+            }
+            catch (Exception ex)
+            {
+                Log.ExceptionOutput(ex, Define.ErrLogPath);
+            }
+            finally
+            {
+                GC.Collect();
+            }
+        }
+
+    }
+}
